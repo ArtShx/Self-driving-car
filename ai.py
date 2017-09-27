d522afd68b027a41cbb5ae2628053869927b1acc
ArtShx <artshx@gmail.com>
2017-09-27 14:48:17 -0300

    Add files via upload
    Brain of the car version 1

--- ai.py
+++ ai.py
@@ -10,15 +10,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
-from autograd import Variable
+from torch.autograd import Variable
 
 # Creating the architecture of the Neural Network
 
 class Network(nn.Module):
+
     def __init__(self, input_size, nb_action):
-        super(Network, self).__init()
+        super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action = nb_action
+        
         """
         fc = full connections (all the neurons on 1 layer will be 
         connected to all the second layer)
@@ -33,48 +35,52 @@ class Network(nn.Module):
         x = F.relu(self.fc1(state))
         q_values = self.fc2(x)
         return q_values
-    
+
 # Implementing Experience Replay
+
 class ReplayMemory(object):
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
-        
+    
     def push(self, event):
         self.memory.append(event)
-        if(len(self.memory) > self.capacity):
+        if len(self.memory) > self.capacity:
             del self.memory[0]
     
     def sample(self, batch_size):
         samples = zip(*random.sample(self.memory, batch_size))
-        # Creates the batches (action, reward, state)
+        # returns the batches (action, reward, state)
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
-    
-# Implementing Deep Q-Learning
+
+# Implementing Deep Q Learning
+
 class Dqn():
+
     def __init__(self, input_size, nb_action, gamma):
-        self.learning_rate = 0.001
         self.gamma = gamma
-        self.reward_model = []
+        self.learning_rate = 0.001
+        self.reward_window = []
         self.model = Network(input_size, nb_action)
         self.memory = ReplayMemory(100000)
         self.optimizer = optim.Adam(self.model.parameters(), lr = self.learning_rate)
-        """
-        # transition states (last_state, last_action, last_reward)
-        # last_state = vector of 5 dimensions(3 signals[straight, left, right],
-                        orientation, -orientation ), which is the input_size
-        # in pytorch it needs to be a Tensor rather than a vector
-        # .unsqueeze(0) is creating a fake dimension corresponding to the batch
-        """
-        self.last_state = torch.Tensor(input_size).unsqueeze(0)
         
-        # 0 = go straight, 1 = go right, 2 = go left
+        '''
+         Transition states (last_state, last_action, last_reward)
+         last_state = vector of 5 dimensions(3 signals[straight, left, right],
+                        orientation, -orientation ), which is the input_size
+         in pytorch it needs to be a Tensor rather than a vector
+         .unsqueeze(0) is creating a fake dimension corresponding to the batch
+        '''
+        
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        # action => 0 = go straight, 1 = go right, 2 = go left
         self.last_action = 0
         self.last_reward = 0
-        
-    # to select the action, we need the input state (neurons) which are the state
-    def select_action(self, state):
-        """
+    
+    '''
+        Select Action function
         Here we need to find out the best action to play in order to get the 
         highest score while still exploring other actions, so we'll use SoftMax
         So we need to generate a distribution of probabilities for each of the
@@ -85,76 +91,105 @@ class Dqn():
         
         We can configure the temperature parameter to set how will the algorithm
         explore the possible actions.
-        """
-        temperature_param = 7
-        probabilities = F.softmax(self.model(Variable(state, volatile = True)) * temperature_param)
+    '''
+    def select_action(self, state):
+        temperature_param = 100
+        probs = F.softmax(self.model(Variable(state, volatile = True))*temperature_param)
         
         # Now we take a random draw of the distribution
-        action = probabilities.multinomial()
+        action = probs.multinomial()
         return action.data[0,0]
     
-    """
-    the Learn function to train ou Neural Network
-    This method is the transition of the Markov decision process that is the base of 
-    Deep Q-Learning
-    This method will get these batches from the ReplayMemory
-    We need the batch_state and the batch_next_state to compute the loss function
-    """
-    
+    '''
+        The Learn function to train the Neural Network
+        This method is the transition of the Markov decision process that is the base of 
+        Deep Q-Learning
+        This method will get these batches from the ReplayMemory
+        We need the batch_state and the batch_next_state to compute the loss function
+    '''
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
-        # We want to get the neuron output of the input state
+        '''
+         We want to get the neuron output of the input state
+
+         self.model(batch_state) returns the output of all the possible actions so
+         we need to get only the one that was decided by the network to play, 
+         so we use the gather function
         
-        # self.model(batch_state) returns the output of all the possible actions so
-        # we need to get only the one that was decided by the network to play, 
-        # so we use the gather function
-        
-        #outputs = what neural network predicts
+         outputs = what our neural network predicts
+        '''
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        output_next = self.model(batch_next_state).detach().max(1)[0]
-        # target is our goal, we want that outputs == target
-        target = self.gamma * output_next + batch_reward
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
         
-        # computing the loss (error of the prediction), calculates the difference between
-        # our prediction and the target
-        # td = temporal difference
-        # smooth_l1_loss is one of the best loss function in deep learning
+        # target is our goal, we want that outputs be equals to target
+        target = self.gamma*next_outputs + batch_reward
+        
+        '''
+         computing the loss (error of the prediction), calculates the difference between
+         our prediction and the target
+         
+         td = temporal difference
+         smooth_l1_loss is one of the best loss function in deep learning
+        '''
         td_loss = F.smooth_l1_loss(outputs, target)
         
         # Back propagation to calculate the stochastic gradient descent updating the weights
         self.optimizer.zero_grad()
         td_loss.backward(retain_variables = True)
         self.optimizer.step()
-        
-
+    
     """"
     Update function - updates all the elements in the transisition as soon as the AI
     reaches a new state (action, state and reward)
+    
+    This method is called when the car moves (enter in sand, change direction, hits the target)
+    and updates the state
 
     This method makes the connection between the AI and the environment
-    """        
+    """  
     def update(self, reward, new_signal):
         new_state = torch.Tensor(new_signal).float().unsqueeze(0)
-        self.memory.push(
-                (self.last_state,
-                 new_state,
-                 torch.LongTensor([int(self.last_action)]),
-                 torch.Tensor([self.last_reward])
-                 ))
+        self.memory.push((self.last_state, 
+                          new_state, 
+                          torch.LongTensor([int(self.last_action)]), 
+                          torch.Tensor([self.last_reward])))
         
+        # Performs the action (take  random samples on memory)
+        action = self.select_action(new_state)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        # Now we need to train and AI learn from the information 
+        # contained in this samples of 100 random transitions
+        nb_transitions = 100
+        if len(self.memory.memory) > nb_transitions:
+            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(nb_transitions)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+         # Reward_window has a fixed size
+        reward_window_size = 1000
+        if len(self.reward_window) > reward_window_size:
+            del self.reward_window[0]
+        return action
+    
+    # Calculates the mean of our rewards
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1.)
+    
+    # Saving our neural network and the optimizer into a file to be able to use later
+    def save(self):
+        torch.save({'state_dict': self.model.state_dict(),
+                    'optimizer' : self.optimizer.state_dict(),
+                   }, 'last_brain.pth')
+    
+    def load(self):
+        # look for the file
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint... ")
+            checkpoint = torch.load('last_brain.pth')
+            # We update our existing model/optimizer to the file that is being loaded
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done !")
+        else:
+            print("no checkpoint found...")
